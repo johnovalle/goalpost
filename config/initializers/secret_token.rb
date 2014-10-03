e70eac1ed850a58327047010eb64795471545589
kugyousha <johnovalle@gmail.com>
2014-10-03 15:53:42 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goalpost::Application.config.secret_key_base = '57aeeedaa821727643dce261eb3192c7d11db78d06b852e33685a6bd2c44a191cabc8a9f06b480ec21149701c5cc788079b98fcad0af43160d707eed7f074f8d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Goalpost::Application.config.session_store :cookie_store, key: '_goalpost_session'
