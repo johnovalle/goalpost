a7e1612da147efe74be3d288046a1dc56cf336d7
kugyousha <johnovalle@gmail.com>
2014-10-03 19:50:59 -0400

    add user model and devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,4 +26,7 @@ Goalpost::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
+  # something Devise asked for
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
