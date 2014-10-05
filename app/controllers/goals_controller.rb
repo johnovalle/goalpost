502a789139d67615a442c8a8873ee3adf21877a7
kugyousha <johnovalle@gmail.com>
2014-10-05 12:21:27 -0400

    finished CRUD

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -9,6 +9,21 @@ class GoalsController < ApplicationController
     end
   end
   
+  def update
+    @goal = Goal.find(params[:id])
+    @goal.update_attributes(goal_params)
+    if @goal.save
+      redirect_to user_path(current_user)
+    else
+      render user_path(current_user)
+    end
+  end
+  
+  def destroy
+    @goal = Goal.find(params[:id]).destroy
+    redirect_to user_path(current_user)
+  end
+  
   private
     def goal_params
       params.require(:goal).permit(:list_id, :name, :complete)
--- app/controllers/steps_controller.rb
+++ app/controllers/steps_controller.rb
@@ -8,6 +8,21 @@ class StepsController < ApplicationController
     end
   end
   
+  def update
+    @step = Step.find(params[:id])
+    @step.update_attributes(step_params)
+    if @step.save
+      redirect_to user_path(current_user)
+    else
+      render user_path(current_user)
+    end
+  end
+  
+  def destroy
+    @step = Step.find(params[:id]).destroy
+    redirect_to user_path(current_user)
+  end
+  
   private
     def step_params
       params.require(:step).permit(:goal_id, :name, :complete)
